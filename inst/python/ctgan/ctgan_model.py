88489c676128115f04d2c6df5aa0182fa2dba85d
kevinykuo <kevinykuo@gmail.com>
2019-11-14 00:20:17 -0500

    bayesian gaussian mixture max_iter=100

--- inst/python/ctgan/ctgan_model.py
+++ inst/python/ctgan/ctgan_model.py
@@ -78,7 +78,7 @@ class DataTransformer(object):
             weight_concentration_prior_type='dirichlet_process',
             weight_concentration_prior=0.001,
             n_init=1,
-            max_iter=1000)
+            max_iter=100)
         gm.fit(data[:, id_].reshape([-1, 1]))
         sys.stdout.flush()
         model.append(gm)
